7899788a312632906c3220a3de44c27898b2dd69
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2021-03-08 13:45:28 +0100

    Create export.js

---
+++ assets/js/export.js
@@ -0,0 +1,8 @@
+// Getting the data //
+var filename = "PrismESP";
+var rawData = base64ToArray(findValue("B64_SRAM_" + filename)).map(function (a) { return String.fromCharCode(a); }).join("");
+				
+var tmpLink = document.createElement("a");
+tmpLink.href = "data:application/force-download," + escape(rawData);
+
+document.body.appendChild(tmpLink);
