c556cff0e7587f4c9dc7deb749661c55c6762dd5
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2021-03-08 14:05:59 +0100

    Update export.js

--- assets/js/export.js
+++ assets/js/export.js
@@ -1,10 +1,7 @@
 // Getting the data //
 var filename = "PrismESP";
-//var rawData = base64ToArray(findValue("B64_SRAM_" + filename)).map(function (a) { return String.fromCharCode(a); }).join("");
-var rawData = "hello"
+var rawData = base64ToArray(findValue("B64_SRAM_" + filename)).map(function (a) { return String.fromCharCode(a); }).join("");
 				
-var tmpLink = document.createElement("a");
+var tmpLink = document.getElementById("downloadSavegame");
 tmpLink.href = "data:application/force-download," + escape(rawData);
 tmpLink.textContent = "Descarga el savegame"
-
-document.body.appendChild(tmpLink);
