69991df43a73bce3626095496cd91ba646ca7299
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2021-03-08 14:18:58 +0100

    Deleted export

--- assets/js/export.js
+++
@@ -1,9 +0,0 @@
-// Getting the data //
-function downloadSavegame() {
-	var filename = document.getElementById("filename").value;
-    var rawData = base64ToArray(findValue("B64_SRAM_" + filename)).map(function (a) { return String.fromCharCode(a); }).join("");
-    			
-    var tmpLink = document.getElementById("downloadSavegame");
-    tmpLink.href = "data:application/force-download," + escape(rawData);
-    tmpLink.textContent = "Descarga el savegame"
-}
